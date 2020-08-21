3101949a59a4f5068612ace15bb69a2e4d3b2eb4
James Rac <james.c.rac@gmail.com>
2020-08-21 03:22:39 -0500

    Updates

---
+++ precache-manifest.92f53867d0c1c73cdaa76cbe1553b5b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18b47d9cdac261917b92e5a1c2717cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52fa312d76d74103b716",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43942beb424cc27993ee",
+    "url": "/static/js/2.9952dcc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9952dcc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fa312d76d74103b716",
+    "url": "/static/js/main.e3e45e37.chunk.js"
+  },
+  {
+    "revision": "730aa632ff897478ce34",
+    "url": "/static/js/runtime-main.291c6430.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
