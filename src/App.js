3b7c46e89dd15880c593bb38d154ed9a383b2e92
James Rac <james.c.rac@gmail.com>
2020-08-24 02:09:28 -0500

    basic layout with scroll enabled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import './App.scss';
+
+function Header(props) {
+  
+
+  const links = props.links.map((val) => {
+    return (<div key={val} className="link" onClick={() => goToSection(val)}>{val}</div>);
+  })
+
+  const goToSection = (id) => {
+    const header = document.querySelector('.header');
+    const anchor = document.querySelector(`#${id}`);
+    if (anchor && header) {
+      window.scrollTo({top: anchor.offsetTop - header.offsetHeight, behavior: 'smooth'});
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <AppBar>
+        <Toolbar className="header">
+          <div className="name">jr3</div>
+          <div className="links">{links}</div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+    </React.Fragment>
+  );
+}
+
+function ContactMe() {
+  return (
+    <div id="Contact">
+      This is where you contact me!
+    </div>
+  );
+}
 
 function App() {
+
+  const links = ["Profile", "Experience", "Projects", "Contact"]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header links={links}></Header>
+      <div id="splash"></div>
+      <div id="Profile">
+        Profile stuff
+      </div>
+      <div id="Experience">
+        experience
+      </div>
+      <div id="Projects">
+        projects
+      </div>
+      <ContactMe></ContactMe>
+    </React.Fragment>
   );
 }
 
