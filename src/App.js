3e9d78d621fa95dd0c7057ed009970b3834896f8
James Rac <james.c.rac@gmail.com>
2020-08-25 15:08:40 -0500

    v1 of careers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -8,6 +7,8 @@ import Avatar from '@material-ui/core/Avatar';
 
 import avatar from './images/avatar.jpg'
 import './App.scss';
+
+import career from './json/career.json';
 
 function Header(props) {
   
@@ -51,9 +52,9 @@ const Profile = () => {
   let lhs = [], rhs = [];
   techs.forEach((tech, index) => {
     if (index % 2 !== 0) {
-      lhs.push(<div className="flex flex--item"><i class="fad fa-angle-right icon"></i>{tech}</div>);
+      lhs.push(<div className="flex flex--item"><i className="fad fa-angle-right icon"></i>{tech}</div>);
     } else {
-      rhs.push(<div className="flex flex--item"><i class="fad fa-angle-right icon"></i>{tech}</div>);
+      rhs.push(<div className="flex flex--item"><i className="fad fa-angle-right icon"></i>{tech}</div>);
     }
   })
 
@@ -72,18 +73,63 @@ const Profile = () => {
           <p>I'm a generalist webapp developer. I'm a senior programmer with skills that emphasize the front end. I love interesting problems and love even more to solve them.
              I believe no matter the skill level, everyone should both mentor and be a mentor to keep improving their skill set.</p>
           <p>Here are some of the technologies I'm currently working with professionally and personally:</p>
-          <p className="flex flex--row">
+          <div className="flex flex--row">
             <div className="flex flex--item flex--column">
               {lhs}
             </div>
             <div className="flex flex--item flex--column">
               {rhs}
             </div>
-          </p>
+          </div>
         </div>
       </div>
     </div>
   );
+}
+
+const Experience = () => {
+  const careers = career.map((job, index) => {
+    const key = `c${index}`
+    const points = job.points.map((point, pindex) => {
+      const pkey = `${key}_p${pindex}`
+      return (<li key={pkey}><span className="fa-li"><i className="fad fa-angle-right"></i></span>{point}</li>);
+    });
+
+    return (
+      <div className="flex--item" key={key}>
+        <div className="">{job.company}, {job.title}</div> 
+        <ul className="fa-ul">
+          {points}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div id="experience" className="fullscreen section">
+      <h2>Experience</h2>
+      <p className="section-description">
+        "Adapt what is useful, reject what is useless, and add what is specifically your own."
+        <br/>- Bruce Lee
+      </p>
+      <hr/>
+      <div className="flex flex--column section-contents">
+        {careers}
+      </div>
+    </div>
+    );
+}
+
+const Projects = () => {
+  return (
+    <div id="projects" className="fullscreen section">
+      <h2>Projects</h2>
+      <p className="section-description">
+        Here's a subdescription
+      </p>
+      <hr/>
+    </div>
+  )
 }
 
 const Splash = () => {
@@ -100,26 +146,18 @@ function App() {
   const theme = createMuiTheme({
     palette: {
       primary: {
-        // light: will be calculated from palette.primary.main,
         main: '#171717',
-        // dark: will be calculated from palette.primary.main,
-        // contrastText: will be calculated to contrast with palette.primary.main
       }
     },
   });
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <CssBaseline /> */}
       <Header links={links}></Header>
       <Splash></Splash>
       <Profile></Profile>
-      <div id="experience">
-        experience
-      </div>
-      <div id="projects">
-        projects
-      </div>
+      <Experience></Experience>
+      <Projects></Projects>
       <ContactMe></ContactMe>
     </ThemeProvider>
   );
